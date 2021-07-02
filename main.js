04b6bf3f66f9173cd657325261e04980a567fedb
amir-reza-tavakkoli <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2021-07-02 23:29:20 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function getInputValue(string) {
+    var inputVal = document.getElementById("field").value;
+    setInputValue("field", string);
+}
+function setInputValue(input_id, val) {
+    let oldValue = document.getElementById(input_id).getAttribute('value');
+    document.getElementById(input_id).setAttribute('value', `${oldValue}${val}`);
+}
+function clearAll(input_id) {
+    document.getElementById(input_id).setAttribute('value', "");
+}
+function calculateAll() {
+    let input_id = "result";
+    var inputVal = document.getElementById("field").value;
+    let x = eval(inputVal);
+    if (x !== undefined) document.getElementById(input_id).setAttribute('value', x);
+
+}
